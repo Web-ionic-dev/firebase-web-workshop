e239e32a30887415126991b9b145975afa0f7959
Somjintana Korbut <nuisomjin@gmail.com>
2019-08-28 12:10:22 +0700

    Remove semicolon ;)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,69 +1,69 @@
-'use strict';
+'use strict' 
 
 /* Set up */
 
 // Checks that the Firebase SDK has been correctly setup and configured.
 function checkSetup() {
     if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
-      window.alert('You have not configured and imported the Firebase SDK. ');
+      window.alert('You have not configured and imported the Firebase SDK. ') 
     }
 }
 
 /* Authentication */
 
 function initFirebaseAuth() {
-    firebase.auth().onAuthStateChanged(authStateObserver);
+    firebase.auth().onAuthStateChanged(authStateObserver) 
 }
 
 function signUp(email, password, name) {
-    console.log('sign up with: ' + email + ' ' + password + ' ' + name);
+    console.log('sign up with: ' + email + ' ' + password + ' ' + name) 
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
         return user.updateProfile({
             displayName: name
         })
     }).catch(function(error) {
-        var errorMessage = error.message;
-        displayAuthError(errorMessage);
-    });
+        var errorMessage = error.message 
+        displayAuthError(errorMessage) 
+    }) 
 }
 
 function signIn(email, password) {
-    console.log('sign in with: ' + email + ' ' + password);
+    console.log('sign in with: ' + email + ' ' + password) 
     firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
-        console.log(user);
+        console.log(user) 
     }).catch(function(error) {
-        var errorMessage = error.message;
-        displayAuthError(errorMessage);
-    });
+        var errorMessage = error.message 
+        displayAuthError(errorMessage) 
+    }) 
     
 }
 
 function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+    const provider = new firebase.auth.GoogleAuthProvider() 
+    firebase.auth().signInWithPopup(provider) 
 }
 
 function signOut() {
-    firebase.auth().signOut();
+    firebase.auth().signOut() 
 }
 
 function getUserId() {
-    return firebase.auth().currentUser.uid;
+    return firebase.auth().currentUser.uid 
 }
 
 function getUserName() {
-    return firebase.auth().currentUser.displayName;
+    return firebase.auth().currentUser.displayName 
 }
 
 function getProfilePicUrl() {
-    return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+    return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png' 
 }
 
 /* Firestore */
 
 function getEvents(filter = 'all') {
 
-    removeAllEventCards();
+    removeAllEventCards() 
 
     var eventList = firebase.firestore().collection('events')
 
@@ -79,7 +79,7 @@ function getEvents(filter = 'all') {
         events.forEach(event => {
             displayEventCard(event.id, event.name, event.startTime, event.description, event.imageUrl)
         })
-    });
+    }) 
 }
 
 function subscribeEvent(eventId) {
@@ -99,8 +99,8 @@ function subscribeEvent(eventId) {
         
         // then display data
         displayEventDetail(eventId, event.name, event.startTime, event.description, event.imageUrl, attendees, isRegistered)
-    });
-    return unsubscribe;
+    }) 
+    return unsubscribe 
 }
 
 function getMyEvents() {
@@ -118,18 +118,18 @@ function getMyEvents() {
         events.forEach(event => {
             displayMyEventItem(event.id, event.name, event.startTime, event.description, event.imageUrl)
         })
-    });
+    }) 
 }
 
 function registerForEvent(eventId) {
-    console.log('register for: ' + eventId);
+    console.log('register for: ' + eventId) 
     const eventRef = firebase.firestore().collection('events').doc(eventId)
     const user =  { profilePicUrl: getProfilePicUrl(), userId: getUserId() }
     eventRef.update({ 'attendees': firebase.firestore.FieldValue.arrayUnion(user)})
 }
 
 function unregisterForEvent(eventId) {
-    console.log('unregister for: ' + eventId);
+    console.log('unregister for: ' + eventId) 
     const eventRef = firebase.firestore().collection('events').doc(eventId)
     const user =  { profilePicUrl: getProfilePicUrl(), userId: getUserId() }
     eventRef.update({ 'attendees': firebase.firestore.FieldValue.arrayRemove(user)})
@@ -138,26 +138,26 @@ function unregisterForEvent(eventId) {
 /* Cloud Messaging */
 
 function requestNotificationsPermissions() {
-    console.log('Requesting notifications permission...');
+    console.log('Requesting notifications permission...') 
     firebase.messaging().requestPermission().then(function() {
       // Notification permission granted.
-      saveMessagingDeviceToken();
+      saveMessagingDeviceToken() 
     }).catch(function(error) {
-      console.error('Unable to get permission to notify.', error);
-    });
+      console.error('Unable to get permission to notify.', error) 
+    }) 
   }
 
 function saveMessagingDeviceToken() {
     firebase.messaging().getToken().then(function(currentToken) {
       if (currentToken) {
-        console.log('Got FCM device token:', currentToken);
+        console.log('Got FCM device token:', currentToken) 
       } else {
         // Need to request permissions to show notifications.
-        requestNotificationsPermissions();
+        requestNotificationsPermissions() 
       }
     }).catch(function(error){
-      console.error('Unable to get messaging token.', error);
-    });
+      console.error('Unable to get messaging token.', error) 
+    }) 
   }
   
 
@@ -168,92 +168,92 @@ function saveMessagingDeviceToken() {
 function initializeAuthUI() {
 
     // Add actions to elements
-    $('#sign-out').click(signOut);
+    $('#sign-out').click(signOut) 
 
     $('#swapToSignUp').click(function(){
-        swapToSignUpMode();
-    });
+        swapToSignUpMode() 
+    }) 
 
     $('#swapToSignIn').click(function(){
-        swapToSignInMode();
-    });
+        swapToSignInMode() 
+    }) 
 
     $('#signin-form').submit(function(){
-        console.log('sign in submmitted');
-        const email = $('#signInInputEmail').val();
-        const password = $('#signInInputPassword').val();
+        console.log('sign in submmitted') 
+        const email = $('#signInInputEmail').val() 
+        const password = $('#signInInputPassword').val() 
         if (email && password) {
-            signIn(email, password);
+            signIn(email, password) 
         } else {
             displayAuthError('Please enter all the required information')
         }
-        event.preventDefault();
+        event.preventDefault() 
 
-    });
+    }) 
 
     $('#signup-form').submit(function(){
-        console.log('sign up submmitted');
-        const email = $('#signUpInputEmail').val();
-        const password = $('#signUpInputPassword').val();
-        const name = $('#signUpInputName').val();
+        console.log('sign up submmitted') 
+        const email = $('#signUpInputEmail').val() 
+        const password = $('#signUpInputPassword').val() 
+        const name = $('#signUpInputName').val() 
         if (email && password && name) {
-            signUp(email, password, name);
+            signUp(email, password, name) 
         } else {
             displayAuthError('Please enter all the required information')
         }
-        event.preventDefault();
-    });
+        event.preventDefault() 
+    }) 
 
     $('#signInWithGoogle').click(function(){
-        hideAuthError();
-        signInWithGoogle();
-    });
+        hideAuthError() 
+        signInWithGoogle() 
+    }) 
 
     $('#signInInputEmail, #signInInputPassword, #signUpInputEmail, #signUpInputPassword, #signUpInputName').change(function() {
-        hideAuthError();
-    });
+        hideAuthError() 
+    }) 
 }
 
 function swapToSignInMode() {
-    $('#signin-form').show();
-    $('#signup-form').hide();
-    $('.modal-title').text('Sign In');
-    $('#error-message').hide();
+    $('#signin-form').show() 
+    $('#signup-form').hide() 
+    $('.modal-title').text('Sign In') 
+    $('#error-message').hide() 
 }
 
 function swapToSignUpMode() {
-    $('#signin-form').hide();
-    $('#signup-form').show();
-    $('.modal-title').text('Sign Up');
-    $('#error-message').hide();
+    $('#signin-form').hide() 
+    $('#signup-form').show() 
+    $('.modal-title').text('Sign Up') 
+    $('#error-message').hide() 
 }
 
 function displayAuthError(err) {
-    var errorMessageDiv = $('#error-message');
-    errorMessageDiv.text(err);
-    errorMessageDiv.show();
+    var errorMessageDiv = $('#error-message') 
+    errorMessageDiv.text(err) 
+    errorMessageDiv.show() 
 }
 
 function hideAuthError() {
     if ($('#error-message').is(':visible')) {
-        $('#error-message').hide();
+        $('#error-message').hide() 
     }
 }
 
 function authStateObserver(user) {
-    console.log('authStateObserver user: ' + user);
+    console.log('authStateObserver user: ' + user) 
     // console.log(JSON.stringify(user))
     if (user) {
         getMyEvents()
-        $('#sign-in').hide();
-        $('#my-event').show();
-        $('#sign-out').show();
+        $('#sign-in').hide() 
+        $('#my-event').show() 
+        $('#sign-out').show() 
     } else {
-        $('#sign-in').show();
-        $('#my-event').hide();
-        $('#sign-out').hide();
+        $('#sign-in').show() 
+        $('#my-event').hide() 
+        $('#sign-out').hide() 
     }
-    $('#authModal').modal('hide');
+    $('#authModal').modal('hide') 
 }
 
 // Events
@@ -269,42 +269,42 @@ const EVENT_TEMPLATE =
             '<p class="description card-text">s</p>'+
         '</div>'+
     '</div>'+
-'</div>';
+'</div>' 
 
 function displayEventCard(id, name, timestamp, description, imageUrl) {
 
     // use existing or create an event card element
-    var div = $('div[data-item-id='+id+']');
+    var div = $('div[data-item-id='+id+']') 
     if (div.length === 0) {
-        div = createEventCard(id, name, timestamp, description, imageUrl);
+        div = createEventCard(id, name, timestamp, description, imageUrl) 
     } 
 
     // set up data
-    div.find('.image').attr('src',  imageUrl ? imageUrl : '/images/temp.png');
-    div.find('.name').text(name);
-    div.find('.date').text(convertedDate(timestamp));
-    div.find('.description').text(description);
+    div.find('.image').attr('src',  imageUrl ? imageUrl : '/images/temp.png') 
+    div.find('.name').text(name) 
+    div.find('.date').text(convertedDate(timestamp)) 
+    div.find('.description').text(description) 
 }
 
-var unsubscribeEventCard;
+var unsubscribeEventCard 
 function createEventCard(id) {
 
     // add event id to div element
-    const div = $(EVENT_TEMPLATE);
-    div.attr('data-item-id', id);
+    const div = $(EVENT_TEMPLATE) 
+    div.attr('data-item-id', id) 
 
     // Add action to card title
-    const cardTitleLabel = div.find('.card-title');
-    cardTitleLabel.attr('data-id', id);
+    const cardTitleLabel = div.find('.card-title') 
+    cardTitleLabel.attr('data-id', id) 
     cardTitleLabel.click(function() {
-        const eventId = $(this).data().id;
-        console.log("See detail for:" + eventId);
+        const eventId = $(this).data().id 
+        console.log("See detail for:" + eventId) 
         unsubscribeEventCard = subscribeEvent(eventId)
-    });
+    }) 
 
     // append event to the event list
-    $('#events').append(div);
-    return div;
+    $('#events').append(div) 
+    return div 
 }
 
 function removeAllEventCards() {
@@ -321,39 +321,39 @@ function removeAllEventCards() {
 // '<h6 class="attendee-title mb-2">Attendees (9)</h6>'+
 // '<div class="attendee-list">'+
 //    '<img src="images/temp.png" class="img-thumbnail rounded float-left">'+
-// '</div>';
+// '</div>' 
 
 function setupEventDetailModal() {
 
     $('#eventDetailModal').on('hidden.bs.modal', function (e) {
         if (unsubscribeEventCard) {
             console.log('unsubscribeEventCard')
-            unsubscribeEventCard();
+            unsubscribeEventCard() 
         }
     })
 }
 
 function displayEventDetail(id, name, timestamp, description, imageUrl, attendees, isRegistered) {
 
-    $('#eventDetailModal .name').text(name);
-    $('#eventDetailModal .image').attr('src',  imageUrl ? imageUrl : '/images/temp.png');
-    $('#eventDetailModal .date').text(convertedDate(timestamp));
-    $('#eventDetailModal .description').text(description);
+    $('#eventDetailModal .name').text(name) 
+    $('#eventDetailModal .image').attr('src',  imageUrl ? imageUrl : '/images/temp.png') 
+    $('#eventDetailModal .date').text(convertedDate(timestamp)) 
+    $('#eventDetailModal .description').text(description) 
 
     displayAttendees(attendees)
 
     // set up register button
-    $('#eventDetailModal .register-button').attr('data-id', id);
-    $('#eventDetailModal .unregister-button').attr('data-id', id);
+    $('#eventDetailModal .register-button').attr('data-id', id) 
+    $('#eventDetailModal .unregister-button').attr('data-id', id) 
 
-    $('#eventDetailModal .register-button').off('click');
+    $('#eventDetailModal .register-button').off('click') 
     $('#eventDetailModal .register-button').on('click', function() {
-        registerForEvent(id);
+        registerForEvent(id) 
     })
 
-    $('#eventDetailModal .unregister-button').off('click');
+    $('#eventDetailModal .unregister-button').off('click') 
     $('#eventDetailModal .unregister-button').on('click', function() {
-        unregisterForEvent(id);
+        unregisterForEvent(id) 
     })
 
     handleRegisterButton(isRegistered)
@@ -362,13 +362,13 @@ function displayEventDetail(id, name, timestamp, description, imageUrl, attendee
 function handleRegisterButton(isRegistered) {
     if (isRegistered) {
         // hide register button (if already registered)
-        $('#eventDetailModal .unregister-button').show();
-        $('#eventDetailModal .register-button').hide();
+        $('#eventDetailModal .unregister-button').show() 
+        $('#eventDetailModal .register-button').hide() 
 
     } else {
         // add action for register button
-        $('#eventDetailModal .register-button').show();
-        $('#eventDetailModal .unregister-button').hide();   
+        $('#eventDetailModal .register-button').show() 
+        $('#eventDetailModal .unregister-button').hide()    
     }
 }
 
@@ -377,25 +377,25 @@ const ATTENDEE_TEMPLATE = '<img src="" class="img-thumbnail rounded float-left">
 function displayAttendees(attendees) {
 
     // attendees
-    $('#eventDetailModal .attendee-title').text('Attendees (' + attendees.length + ')');
+    $('#eventDetailModal .attendee-title').text('Attendees (' + attendees.length + ')') 
 
     // remove all attendee from the list (if any)
     $('#eventDetailModal .attendee-list').children().each(function(i) {
-        this.remove();
+        this.remove() 
     })
 
     // display attendee profile pic (if needed)
     if (attendees.length > 0) {
         attendees.forEach(attendee => {
             displayAttendeeProfilePic(attendee.profilePicUrl)
-        });
+        }) 
     }
 }
 
 function displayAttendeeProfilePic(imageUrl) {
 
-    const img = $(ATTENDEE_TEMPLATE);
-    img.attr('src', imageUrl);
+    const img = $(ATTENDEE_TEMPLATE) 
+    img.attr('src', imageUrl) 
     $('.modal-body .attendee-list').append(img)
 }
 
@@ -408,31 +408,31 @@ const MY_EVENT_TEMPLATE =
         '<h6 class="date mb-2 text-muted"></h6>'+
         '<p class="description"></p>'+
     '</div>'+
-'</div>';
+'</div>' 
 
 function displayMyEventItem(id, name, timestamp, description, imageUrl) {
     // use existing or create an event card element
-    var div = $('div[data-item-id='+id+']');
+    var div = $('div[data-item-id='+id+']') 
     if (div.length === 0) {
-        div = createMyEventItem(id);
+        div = createMyEventItem(id) 
     } 
 
     // set up data
-    div.find('.image').attr('src', imageUrl ? imageUrl : '/images/temp.png');
-    div.find('.name').text(name);
-    div.find('.date').text(convertedDate(timestamp));
-    div.find('.description').text(description);
+    div.find('.image').attr('src', imageUrl ? imageUrl : '/images/temp.png') 
+    div.find('.name').text(name) 
+    div.find('.date').text(convertedDate(timestamp)) 
+    div.find('.description').text(description) 
 }
 
 function createMyEventItem(id) {
 
     // add event id to div element
-    const div = $(MY_EVENT_TEMPLATE);
-    div.attr('data-item-id', id);
+    const div = $(MY_EVENT_TEMPLATE) 
+    div.attr('data-item-id', id) 
 
     // append event to the event list
-    $('#my-events').append(div);
-    return div;
+    $('#my-events').append(div) 
+    return div 
 }
 
 function convertedDate(timestamp) {
@@ -446,32 +446,32 @@ function addActionsForDropdownMenu() {
 
     // add actions for dropdowm
     $('#typeDropdownMenu').change(function() {
-        handleForDropdownChanged();
-    });
+        handleForDropdownChanged() 
+    }) 
 
     $('#timeDropdownMenu').change(function() {
-        handleForDropdownChanged();
-    });
+        handleForDropdownChanged() 
+    }) 
 }
 
 function handleForDropdownChanged() {
-    const type = $('#typeDropdownMenu').val();
-    const time = $('#timeDropdownMenu').val();
-    getEvents(type);
+    const type = $('#typeDropdownMenu').val() 
+    const time = $('#timeDropdownMenu').val() 
+    getEvents(type) 
 }
 
 function loadIncludes(callback) {
-    var deferreds = [];
+    var deferreds = [] 
     // Create a deferred for all includes
     $("[data-load]").each(function() {
-        const d = new $.Deferred();
-        deferreds.push(d);
+        const d = new $.Deferred() 
+        deferreds.push(d) 
         $(this).load($(this).data("load"), function() {
-            d.resolve();
-        });
-    });
+            d.resolve() 
+        }) 
+    }) 
     // Callback when all deferreds are done
-    $.when.apply(null, deferreds).done(callback);
+    $.when.apply(null, deferreds).done(callback) 
 }
 
 /* Main */
@@ -479,22 +479,22 @@ function loadIncludes(callback) {
 $(document).ready(function() {
     loadIncludes(function() {
         // initialize Firebase
-        initializeAuthUI();
-        initFirebaseAuth();
-        setupEventDetailModal();
+        initializeAuthUI() 
+        initFirebaseAuth() 
+        setupEventDetailModal() 
     })
-});
+}) 
 
-addActionsForDropdownMenu();
+addActionsForDropdownMenu() 
 
 // isUserSignIn()
 
 // getUID()
 
-// TODO: checkSetup();
+// TODO: checkSetup() 
 
-getEvents();
+getEvents() 
 
-// loadMyEvents();
+// loadMyEvents() 
 
-requestNotificationsPermissions();
+requestNotificationsPermissions() 
