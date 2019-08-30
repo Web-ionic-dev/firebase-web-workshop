b66f3273e11cae8737da6f364f77ba99d220b958
Somjintana Korbut <nuisomjin@gmail.com>
2019-08-30 16:20:54 +0700

    Remove service worker file from starter project

--- starter/public/firebase-messaging-sw.js
+++
@@ -1,6 +0,0 @@
-importScripts('/__/firebase/6.3.4/firebase-app.js');
-importScripts('/__/firebase/6.3.4/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
-
-firebase.messaging();
-console.log('init message');
